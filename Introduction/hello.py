a72aed08f7310fe62e4033c75fa1e13a311e1191
Jessica <jtstodola@gmail.com>
2015-11-23 17:47:20 -0600

    Learned printing and getting input from a user

---
+++ Introduction/hello.py
@@ -0,0 +1,4 @@
+print "Enter your first name: "
+first_name = raw_input()
+
+print "Hello, %s, nice to meet you!" % first_name
---
+++ Introduction/number_of_characters.py
@@ -0,0 +1,6 @@
+print "What is the input string?"
+input_string = raw_input()
+
+characters = len(input_string)
+
+print "The string %s has %i characters." % (input_string, characters)
